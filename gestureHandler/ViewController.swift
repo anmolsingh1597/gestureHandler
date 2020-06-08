fc929b5e81d79996bb606ae220a67f4037be99f3
Anmol Singh <anmolsingh1597@gmail.com>
2020-06-08 19:28:02 -0400

    Initial Commit

---
+++ gestureHandler/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gestureHandler
+//
+//  Created by Anmol singh on 2020-06-08.
+//  Copyright © 2020 Swift Project. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
